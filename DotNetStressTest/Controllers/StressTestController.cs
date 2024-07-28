d8b3647fe7a5e79ce67f94c676b8dce4f7ffef4b
Abhishek Jha <abhimasum@gmail.com>
2024-07-28 23:14:46 +0530

    added code for stress test

---
+++ DotNetStressTest/Controllers/StressTestController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetStressTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StressTestController : ControllerBase
+    {
+        private static bool finish = false;
+
+        public StressTestController()
+        {
+           
+        }
+
+        [HttpGet(Name = "GetStressTest")]
+        public string Get(bool stoprunning=false)
+        {
+            finish = stoprunning;
+            for (int i = 0;; i++)
+            {                
+                if (finish)
+                    break;               
+            }
+            return "Running Stops";
+        }
+
+        
+    }
+}
